9c5534e3e5319f0fc470777692297d1c2e3abcad
extasy44 <seoheejun@gmail.com>
2020-11-21 18:37:46 +1100

    styled app bar

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+
+const App = () => {
+  return (
+    <AppLayout>
+      <AppBar />
+      Crypto Dashboard
+    </AppLayout>
+  );
+};
+
+export default App;
