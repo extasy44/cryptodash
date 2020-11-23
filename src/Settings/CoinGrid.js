425338140a297bfb282e01f10d59167ac87e873b
extasy44 <seoheejun@gmail.com>
2020-11-23 23:27:14 +1100

    create coinGrid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import styled, { css } from 'styled-components';
 import { AppContext } from '../App/AppProvider';
+import { SelectableTile } from '../Shared/Tiles';
 
 export const CoinGridStyled = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
 `;
 
 const CoinGrid = () => {
@@ -13,8 +15,8 @@ const CoinGrid = () => {
   console.log(coinList);
   return (
     <CoinGridStyled>
-      {Object.keys(coinList).map((coinKey) => {
-        return <div>{coinKey}</div>;
+      {Object.keys(coinList).map((coinKey, index) => {
+        return <SelectableTile key={index}>{coinKey}</SelectableTile>;
       })}
     </CoinGridStyled>
   );
